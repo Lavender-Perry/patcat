b15e43bb6405cbfd6e5ef4b2ec9508797e16da90
Lavender Perry <endie2@protonmail.com>
2021-09-09 19:54:45 -0700

    PyPI packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = "pat",
-    version = "2021.09.07",
-    scripts = ["pat.py"]
+    version = "2021.09.09",
+    description = "Outputs text with rainbow colors.",
+    url = "https://gitdab.com/endie2/pat",
+    author = "Lavender Perry",
+    author_email = "endie2@protonmail.com",
+    packages = ["pat"],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities"
+    ]
 )
