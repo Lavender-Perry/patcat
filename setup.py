724389c0aa3340ab9af884e9aa910bafd443345a
Lavender Perry <endie2@protonmail.com>
2021-11-21 20:21:41 -0800

    Move pat.py to pat, update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name = "patcat",
-    version = "2021.09.10.2",
+    version = "devel",
     description = "Outputs text with rainbow colors.",
     long_description = open("README").read(),
     long_description_content_type = "text/markdown",
     url = "https://gitdab.com/endie2/pat",
     author = "Lavender Perry",
     author_email = "endie2@protonmail.com",
-    scripts = ["pat.py"],
+    scripts = ["pat"],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
